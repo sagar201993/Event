09dff6615bdbe1540cf1078bbf42d013fdc398da
sagar201993 <sagarvirk1@gmail.com>
2024-07-26 13:52:55 +0200

    first commit

---
+++ components/shared/Header.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            width={128}
+            height={38}
+            alt="event logo"
+          />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSwitchSessionUrl="/" />
+          </SignedIn>
+
+          <SignedOut>
+            <Button asChild className="rounded-full " size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
