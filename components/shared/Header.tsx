3b69182d8a1da901e9af09638bbe86b80aa4975b
sagar201993 <sagarvirk1@gmail.com>
2024-07-26 14:10:22 +0200

    implemented nav and mobile link

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,6 +3,8 @@ import Link from "next/link";
 import Image from "next/image";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 const Header = () => {
   return (
     <header className="w-full border-b">
@@ -15,9 +17,16 @@ const Header = () => {
             alt="event logo"
           />
         </Link>
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSwitchSessionUrl="/" />
+            <MobileNav />
           </SignedIn>
 
           <SignedOut>
